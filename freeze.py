99645ad4acb572e553d32f15784cb97a6f5f9877
laurirasanen <lauri@rasanen.dev>
2024-01-18 02:10:14 +0200

    Avoid switching model when freezing

--- freeze.py
+++ freeze.py
@@ -9,13 +9,28 @@ freezer = Freezer(app)
 @freezer.register_generator
 def player_page():
     players = get_players()
+
+    # yield classes in separate loops so we don't swap models twice per player
     print(
-        f"generating {len(players)} player pages (ids: {players[0]['id']} - {players[-1]['id']})"
+        f"generating {len(players)} soldier player pages (ids: {players[0]['id']} - {players[-1]['id']})"
     )
     x = 0
     time_start = time.time()
     for player in players:
         yield {"player_id": player["id"], "class_id": 3}
+        x += 1
+        if x % 50 == 0:
+            elapsed = time.time() - time_start
+            print(
+                f"progress: {x}/{len(players)}, elapsed: {round(elapsed)}s, eta: {round((elapsed / x) * len(players) - elapsed)}s"
+            )
+
+    print(
+        f"generating {len(players)} demoman player pages (ids: {players[0]['id']} - {players[-1]['id']})"
+    )
+    x = 0
+    time_start = time.time()
+    for player in players:
         yield {"player_id": player["id"], "class_id": 4}
         x += 1
         if x % 50 == 0:
@@ -24,16 +39,31 @@ def player_page():
                 f"progress: {x}/{len(players)}, elapsed: {round(elapsed)}s, eta: {round((elapsed / x) * len(players) - elapsed)}s"
             )
 
+
 @freezer.register_generator
 def map_page():
     maps = get_maps()
+
     print(
-        f"generating {len(maps)} map pages"
+        f"generating {len(maps)} map pages for soldier"
     )
     x = 0
     time_start = time.time()
     for map in maps:
         yield {"map_name": map["name"], "class_id": 3}
+        x += 1
+        if x % 50 == 0:
+            elapsed = time.time() - time_start
+            print(
+                f"progress: {x}/{len(maps)}, elapsed: {round(elapsed)}s, eta: {round((elapsed / x) * len(maps) - elapsed)}s"
+            )
+
+    print(
+        f"generating {len(maps)} map pages for demoman"
+    )
+    x = 0
+    time_start = time.time()
+    for map in maps:
         yield {"map_name": map["name"], "class_id": 4}
         x += 1
         if x % 50 == 0:
